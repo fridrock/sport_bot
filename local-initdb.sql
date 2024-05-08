aa7ebebd5307942c44aa977bc82d1a47dff7d5a6
Александр <fridrix.aleksandr.2004@mail.ru>
2024-05-08 18:58:36 +0300

    change database to all services

--- local-initdb.sql
+++ local-initdb.sql
@@ -1,3 +1,9 @@
+-- For users service
 CREATE USER lenchik WITH PASSWORD 'cgovno';
 CREATE DATABASE sport_bot ENCODING 'UTF-8';
 GRANT all privileges on DATABASE sport_bot to lenchik;
+
+-- For training service
+CREATE USER trainingservice WITH PASSWORD 'root';
+CREATE DATABASE training_db ENCODING 'UTF-8';
+GRANT all privileges on DATABASE training_db to trainingservice;
